eda8d068eead346363d8736e8fa3310f06c12c7f
Motard <pmotard@sapo.pt>
2014-01-13 12:29:30 +0000

    AmbulareApplication
    Implementar a application propria para controlar o estado da app

--- Ambulare/src/com/example/ambulare/AmbulareApplication.java
+++ Ambulare/src/com/example/ambulare/AmbulareApplication.java
@@ -2,13 +2,26 @@ package com.example.ambulare;
 
 import android.app.Application;
 
-public class AmbulareApplication extends Application {
+public class AmbulareApplication extends Application 
+{
 	
+	private boolean _recording;
 	
 	@Override
-	public void onCreate() {
-		// TODO Auto-generated method stub
+	public void onCreate() 
+	{
 		super.onCreate();
+		_recording = false;
+	}
+
+	public synchronized boolean isRecordingRoute()
+	{
+		return _recording;
+	}
+	
+	public synchronized void setRecording(boolean state)
+	{
+		_recording = state;
 	}
 
 }
--- Ambulare/src/com/example/ambulare/MainActivity.java
+++ Ambulare/src/com/example/ambulare/MainActivity.java
@@ -12,8 +12,11 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,30 +29,43 @@ public class MainActivity extends Activity {
 		relativeLayout = (RelativeLayout)findViewById(R.id.layout_main);
 		relativeLayout.setBackgroundColor(Color.DKGRAY);
         
-//		Change activity to get gps position
-		findViewById(R.id.main_bt_write_location).setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
+//		Ver o estado do recording
+		AmbulareApplication app = (AmbulareApplication) getApplication();
+		if (!app.isRecordingRoute())
+		{
+		
+	//		Change activity to get gps position
+			findViewById(R.id.main_bt_write_location).setOnClickListener(new View.OnClickListener() {
 				
-				final EditText editText = new EditText(MainActivity.this);
-				new AlertDialog.Builder(MainActivity.this).setTitle("Nova Rota")
-				 .setNegativeButton("cancel", null)
-				 .setPositiveButton("add", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(View v) {
 					
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						Intent mudaEcra = new Intent(MainActivity.this, GetLocation.class);
-						mudaEcra.putExtra(GetLocation.NOME_ROTA, editText.getText().toString());
-						startActivity(mudaEcra);
-//						startService(intent);
-					}
-				})
-				.setView(editText)// Returns a Builder
-				.create() //Returns an AlertDialog
-				.show();
-			}
-		});
+					final EditText editText = new EditText(MainActivity.this);
+					new AlertDialog.Builder(MainActivity.this).setTitle("Nova Rota")
+					 .setNegativeButton("cancel", null)
+					 .setPositiveButton("add", new DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							Intent mudaEcra = new Intent(MainActivity.this, GetLocation.class);
+							mudaEcra.putExtra(GetLocation.NOME_ROTA, editText.getText().toString());
+							startActivity(mudaEcra);
+	//						startService(intent);
+							((AmbulareApplication) getApplication()).setRecording(true);
+						}
+					})
+					.setView(editText)// Returns a Builder
+					.create() //Returns an AlertDialog
+					.show();
+				}
+			});
+		}
+		else
+		{
+			((TextView)findViewById(R.id.main_bt_write_location)).setText("STOP");
+			app.setRecording(false);
+		}
+		
 	}
 
 	@Override

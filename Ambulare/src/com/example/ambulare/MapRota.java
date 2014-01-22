6a8bd89888e6e702edf78fe49d128a3c4980becf
Motard <pmotard@sapo.pt>
2014-01-22 12:25:31 +0000

    MapRota

---
+++ Ambulare/src/com/example/ambulare/MapRota.java
@@ -0,0 +1,18 @@
+package com.example.ambulare;
+
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+
+public class MapRota extends FragmentActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map_rota);
+	}
+
+}
